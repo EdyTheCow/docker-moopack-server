5dbf9de3a1fc051e10e76fc33a43b1de5af3109c
EdyTheCow <github@edyyy.com>
2018-06-17 18:53:56 +0200

    adding config files

--- common/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# server.properties
-sed -i s/MC_MAX_PLAYERS/$MC_MAX_PLAYERS/g /home/mc/server/server.properties
-sed -i s/MC_PORT/$MC_PORT/g /home/mc/server/server.properties
-sed -i s/MC_VIEW_DISTANCE/$MC_VIEW_DISTANCE/g /home/mc/server/server.properties
-
-/usr/bin/java -Xms$MC_MIN_RAM -Xmx$MC_MAX_RAM -jar /home/mc/common/spigot/$MC_VERSION.jar
